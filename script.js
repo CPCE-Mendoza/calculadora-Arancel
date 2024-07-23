5c8726056562b23268c387dc95be141f50b7c42c
CPCE-Mendoza <149011804+CPCE-Mendoza@users.noreply.github.com>
2024-07-23 16:24:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,29 +34,44 @@ document.getElementById('enviarMail').addEventListener('click', function() {
   var recipient = prompt("Introduce el email del destinatario:", "destinatario@example.com");
 
   if (recipient) {
-      var url = 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec'; // Reemplaza con el ID real
+    var url = 'https://script.google.com/macros/s/AKfycbw3AqYbVEEF5ByaCxRfrtPo_xafEVM7d0vGGlZ52unDtvbifwTAMHKa2XJNgz_u_jAr0w/exec'; // Asegúrate de usar el ID correcto
 
-      var payload = {
-          'recipient': recipient,
-          'subject': 'Resultado del Cálculo',
-          'body': `Importe Base de Búsqueda: $${formattedImporteBase}\nArancel Fijo: $${formattedArancel}`
-      };
+    var payload = {
+      'recipient': recipient,
+      'subject': 'Resultado Calculadora de Arancel CPCE Mendoza',
+      'body': `Importe Base de Búsqueda: $${formattedImporteBase}\nArancel Fijo: $${formattedArancel}
+El arancel es para los siguientes trámites:\n
+ESTADOS CONTABLES SOC. COMERCIALES\n
+ESTADOS CONTABLES ENTIDADES SIN F/ LUCROS\n
+ESTADOS CONTABLES DE COOPERATIVA\n
+ESTADOS CONTABLES INTERMEDIOS\n
+COMPILACIÓN\n
+ESTADOS CONTABLES ESPECIALES\n
+RECTIFICATIVO - ESTADOS CONTABLES SOC. COMERCIALES\n
+RECTIFICATIVO - ESTADOS CONTABLES ENTIDADES SIN F/ LUCROS\n
+RECTIFICATIVO - ESTADOS CONTABLES DE COOPERATIVA\n
+RECTIFICATIVO - ESTADOS CONTABLES INTERMEDIOS\n
+RECTIFICATIVO - COMPILACIÓN\n
+RECTIFICATIVO - ESTADOS CONTABLES ESPECIALES\n
+Incluye dos trámites con diferentes destinatarios.\n
+Te esperamos en Mi Cuenta para gestionar el trámite`
+    };
 
-      fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          body: new URLSearchParams(payload).toString() // Usa URLSearchParams para manejar el formato
-      })
-      .then(response => response.text())
-      .then(result => {
-          alert('Correo enviado exitosamente');
-      })
-      .catch(error => {
-          console.error('Error:', error);
-          alert('Error al enviar el correo');
-      });
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams(payload).toString() // Usa URLSearchParams para manejar el formato
+    })
+    .then(response => response.text())
+    .then(result => {
+      alert('Correo enviado exitosamente');
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Error al enviar el correo');
+    });
   }
 });
 
@@ -98,7 +113,22 @@ function calcularArancel(activo, pasivo, ingresos) {
 
   document.getElementById('resultado').innerHTML =
     '<p><b>Importe Base de Búsqueda:</b> $' + formattedImporteBase + '</p>' +
-    '<p><b>Arancel Fijo:</b> $' + formattedArancel + '</p>';
+    '<p><b>Arancel Fijo:</b> $' + formattedArancel + '</p>' +
+    '<p>El arancel es para los siguientes trámites:</p>' +
+    '<p>ESTADOS CONTABLES SOC. COMERCIALES</p>' +
+    '<p>ESTADOS CONTABLES ENTIDADES SIN F/ LUCROS</p>' +
+    '<p>ESTADOS CONTABLES DE COOPERATIVA</p>' +
+    '<p>ESTADOS CONTABLES INTERMEDIOS</p>' +
+    '<p>COMPILACIÓN</p>' +
+    '<p>ESTADOS CONTABLES ESPECIALES</p>' +
+    '<p>RECTIFICATIVO - ESTADOS CONTABLES SOC. COMERCIALES</p>' +
+    '<p>RECTIFICATIVO - ESTADOS CONTABLES ENTIDADES SIN F/ LUCROS</p>' +
+    '<p>RECTIFICATIVO - ESTADOS CONTABLES DE COOPERATIVA</p>' +
+    '<p>RECTIFICATIVO - ESTADOS CONTABLES INTERMEDIOS</p>' +
+    '<p>RECTIFICATIVO - COMPILACIÓN</p>' +
+    '<p>RECTIFICATIVO - ESTADOS CONTABLES ESPECIALES</p>' +
+    '<p>Incluye dos trámites con diferentes destinatarios.</p>' +
+    '<p>Te esperamos en Mi Cuenta para gestionar el trámite</p>';
 }
 
 function formatNumber(number) {
